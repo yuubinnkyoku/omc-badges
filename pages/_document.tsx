548030c371e62597b614da6f3464e0ee08d28b71
yuubinnkyoku <yuubinnkyokuda0730@gmail.com>
2025-08-04 14:17:07 +0900

    OMCドキュメントにアイコンリンクを追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ export default class extends Document {
     render() {
         return (
             <Html lang="ja">
-                <Head />
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
